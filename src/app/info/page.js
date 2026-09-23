2385246263987fd538f1b9780afb37fc0d6fb8e7
unknown <pinela@gmail.com>
2026-09-23 17:38:57 +0100

    fixed link module ref

--- src/app/info/page.js
+++ src/app/info/page.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./page.module.css";
+import styles from "./../page.module.css";
 
 export default function Info() {
   return (
     <div className={styles.page}>
         <p>
-            hello there!
+            HJello there! Welcome!
         </p>
           <div>
             <Link href="/">Home página</Link>
