578c9194a576901066a697e984bcc5c856cc024a
unknown <pinela@gmail.com>
2026-09-24 15:08:45 +0100

    added title env label

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Amplify Test",
+  title: `Amplify Test - ${process.env.ENVTYPE || "Prod"} `,
   description: "Test this s*it",
 };
 
